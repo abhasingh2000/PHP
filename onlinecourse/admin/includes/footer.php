a4e53daf8d6674f1d51494b711e9c368d4895071
abhasingh2000 <abhasingh00522@gmail.com>
2021-01-25 22:49:52 -0800

    Php Project

---
+++ onlinecourse/admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                     Developed By Akash | Abhi | Shrivastava | Abha | &copy; 2021 Php Project 
+                </div>
+
+            </div>
+        </div>
+    </footer>
